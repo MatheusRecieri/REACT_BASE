fad9b14475cb424ca74f91c4e9fa5236950ed08f
Matheus de Sa Recieri <matheus.recieri@grupofs.local>
2025-10-06 11:54:16 -0300

    Configurações da base do projeto

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import babelParser from '@babel/eslint-parser';
+
+export default [
+  // Config padrão JS do ESLint
+  js.configs.recommended,
+
+  // Config React
+  pluginReact.configs.flat.recommended,
+
+  // Config personalizada do projeto
+  {
+    files: ['**/*.{js,jsx,mjs,cjs}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: { presets: ['@babel/preset-react'] },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReact,
+      'react-hooks': reactHooksPlugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      // Prettier como erro
+      'prettier/prettier': 'error',
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-filename-extension': 0,
+
+      // React Hooks
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // JS padrão
+      semi: ['error', 'always'],
+      'prefer-const': ['error'],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+];
